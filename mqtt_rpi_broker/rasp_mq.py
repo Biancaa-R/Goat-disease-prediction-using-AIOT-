6d767191b911cb4ca8aa11ef94e5b33b1a2a982e
Biancaa Ramesh <87770461+Biancaa-R@users.noreply.github.com>
2024-08-25 07:51:07 +0100

    Add files via upload

--- mqtt_rpi_broker/rasp_mq.py
+++ mqtt_rpi_broker/rasp_mq.py
@@ -12,14 +12,40 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(topic)
 
 def on_message(client, userdata, msg):
-     print("Message received: " + msg.payload.decode())
+    global antharax_det
+    global tetanus_det
+     #print("Message received: " + msg.payload.decode())
+    print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
+    message= list(msg.payload.decode());
+    pulse=message[0]
+    motion=message[1]
+    temp=message[2]
+    #list1=[pulse,motion,temp]
+    #70–130 beats per minute.
+    if (pulse>140):
+        tetanus_det=1
+    if (temp>40):
+        antharax_det=1
+    #38.5–39.7
 #creation of client object
 client.on_connect=on_connect
 client.on_message=on_message
 client.connect(hostname, broker_port, 60)
-message = str(random.randint(1, 100))
-client.publish(topic, message)
+start=time.time.now()
+#message = str(random.randint(1, 100))
+#client.publish(topic, message)
+while(True):
+    client.loop_start()
+    stop=time.time.now()
+    if (stop-start> 7200):
+        if (antharax_det==1):
+            pass
+        if(tetanus_det==1):
+            pass
+        start=time.time.now()
+        antharax_det=0
+        tetanus_det=0
 
-client.loop_start()
-#Start the MQTT client's network loop by calling the loop_start() method to begin receiving messages.\
+    #Start the MQTT client's network loop by calling the loop_start() method to begin receiving messages.\
+
 

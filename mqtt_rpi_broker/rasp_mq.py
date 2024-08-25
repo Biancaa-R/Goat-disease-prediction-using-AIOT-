a540be0a8fa0267b9c55d12f1ed64ca33d435707
Biancaa Ramesh <87770461+Biancaa-R@users.noreply.github.com>
2024-08-25 15:15:39 +0100

    Add files via upload

--- mqtt_rpi_broker/rasp_mq.py
+++ mqtt_rpi_broker/rasp_mq.py
@@ -2,9 +2,9 @@
 import time
 import random
 import paho.mqtt.client as mqtt
-hostname = "raspberrypi" #should be local host or ip address of rpi
+hostname = "localhost"
 broker_port = 1883
-topic = "testtopic"
+topic = "esp8266"
 client = mqtt.Client()
 
 def on_connect(client, userdata, flags, rc):
@@ -16,33 +16,33 @@ def on_message(client, userdata, msg):
     global tetanus_det
      #print("Message received: " + msg.payload.decode())
     print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
-    message= list(msg.payload.decode());
-    pulse=message[0]
+    message= (msg.payload.decode()).split(",");
+    temp=message[0]
     motion=message[1]
-    temp=message[2]
+    pulse=message[2]
     #list1=[pulse,motion,temp]
     #70–130 beats per minute.
-    if (pulse>140):
+    if (int(pulse)>140):
         tetanus_det=1
-    if (temp>40):
+    if (float(temp)>40):
         antharax_det=1
     #38.5–39.7
 #creation of client object
 client.on_connect=on_connect
 client.on_message=on_message
 client.connect(hostname, broker_port, 60)
-start=time.time.now()
+start=time.time()
 #message = str(random.randint(1, 100))
 #client.publish(topic, message)
 while(True):
     client.loop_start()
-    stop=time.time.now()
+    stop=time.time()
     if (stop-start> 7200):
         if (antharax_det==1):
             pass
         if(tetanus_det==1):
             pass
-        start=time.time.now()
+        start=time.time()
         antharax_det=0
         tetanus_det=0
 
